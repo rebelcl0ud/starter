68166785388d695007485ceeb228c9bf06c755d9
rebelcl0ud <jovanka.placeres@gmail.com>
2019-02-05 16:25:14 -0800

    add: gulp-sass, dev-dependency; test files to test running gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('printName', function() {
-	console.log('jo')
-})
+// gulp.task('printName', function() {
+// 	console.log('jo')
+// })
 
-gulp.task('printAge', function() {
-	console.log('30')
-})
+// gulp.task('printAge', function() {
+// 	console.log('30')
+// })
 
-gulp.task('default',['printName', 'printAge'])
+// sass
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+// gulp.task('default',['printName', 'printAge'])
