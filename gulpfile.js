9baeacd9bf3d2f0a53eaba66c9b2acef42db6607
rebelcl0ud <jovanka.placeres@gmail.com>
2019-02-06 18:03:57 -0800

    add: gulp-exec runs webpack; alternative to gulp-shell, seems use of anti-pattern has put it in the dont use zone

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,14 @@ const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 let cleanCSS = require('gulp-clean-css');
 let sourcemaps = require('gulp-sourcemaps');
+let exec = require('gulp-exec');
 
+// webpack
+gulp.task('webpack', function() {
+  return gulp.src('*.js', {read: false})
+    .pipe(exec('webpack'))
+    .pipe(browserSync.stream())
+})
 
 // sass
 gulp.task('sass', function () {
@@ -40,15 +47,18 @@ gulp.task('browser-sync', function() {
       baseDir: "./public",
       notify: false,
       // when true opens new window each 'gulp' run
-      open: false
+      open: false,
     }
   });
 });
 
-
-
-gulp.task('default',['sass', 'browser-sync'], function() {
-	gulp.watch('./src/scss/**/*', ['sass']);
+gulp.task('default',['sass', 'webpack', 'browser-sync'], function() {
+	gulp.watch('./src/scss/**/*', ['sass'])
+  gulp.watch('./src/js/**/*', ['webpack'])
 })
 
 gulp.task('production', ['sass:minify'])
+
+
+
+
